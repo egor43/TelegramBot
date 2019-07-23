75d06e7e127dc57d0d6c68b20b2a66a8cbdbaa2c
ev.myshko <ev.myshko@tensor.ru>
2019-07-23 18:21:15 +0300

    Update message module

--- src/TelegramObjects/message.py
+++ src/TelegramObjects/message.py
@@ -8,15 +8,40 @@ author: Myshko E.V.
 
 class Message():
     """
-    Describes the message data from chat update
+    This object is a message.
     """
 
     message_id = None
-    user = None  # The user who sent the message
+    user = None
     date = None
-    entities = None
-    text = None
     chat = None
+    forward_from = None
+    forward_date = None
+    reply_to_message = None
+    text = None
+    entities = None
+    audio = None
+    document = None
+    photo = None
+    sticker = None
+    video = None
+    voice = None
+    caption = None
+    contact = None
+    location = None
+    venue = None
+    new_chat_member = None
+    left_chat_member = None
+    new_chat_title = None
+    new_chat_photo = None
+    delete_chat_photo = None
+    group_chat_created = None
+    supergroup_chat_created = None
+    channel_chat_created = None
+    migrate_to_chat_id = None
+    migrate_from_chat_id = None
+    pinned_message = None
+
 
     def __init__(self, message_data: dict):
         """
