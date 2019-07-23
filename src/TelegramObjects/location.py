de880cb9e01d29e1d0414fca8276e1a9a8ccb994
ev.myshko <ev.myshko@tensor.ru>
2019-07-23 17:59:28 +0300

    Add location module

---
+++ src/TelegramObjects/location.py
@@ -0,0 +1,23 @@
+"""
+The class describes the Location object from Telegram API
+https://tlgrm.ru/docs/bots/api#location
+
+author: Myshko E.V.
+"""
+
+
+class Location():
+    """
+    This object represents a point on the map.
+    """
+
+    longitude = None
+    latitude = None
+
+
+    def __init__(self, location_data: dict):
+        """
+        Object initialization
+        :param location_data: The location data that was received from the update object api telegram
+        """
+        pass
