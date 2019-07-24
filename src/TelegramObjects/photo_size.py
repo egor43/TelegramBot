3cdc462b633010bf099cd8bf323275bb2cff0dbd
ev.myshko <ev.myshko@tensor.ru>
2019-07-24 15:59:57 +0300

    Add module photo_size

---
+++ src/TelegramObjects/photo_size.py
@@ -0,0 +1,25 @@
+"""
+The class describes the PhotoSize object from Telegram API
+https://tlgrm.ru/docs/bots/api#photosize
+
+author: Myshko E.V.
+"""
+
+
+class PhotoSize():
+    """
+    This object represents an image of a certain size or a preview of a file / sticker.
+    """
+
+    file_id = None
+    width = None
+    height = None
+    file_size = None
+
+
+    def __init__(self, photo_size_data: dict):
+        """
+        Object initialization
+        :param message_entity_data: The photo size data that was received from the update object api telegram
+        """
+        pass
