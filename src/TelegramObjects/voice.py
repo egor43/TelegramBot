eea0d1b3004e46de881034359d652b866bd4dad5
ev.myshko <ev.myshko@tensor.ru>
2019-07-24 16:13:17 +0300

    add voice module

---
+++ src/TelegramObjects/voice.py
@@ -0,0 +1,25 @@
+"""
+The class describes the Voice object from Telegram API
+https://tlgrm.ru/docs/bots/api#video
+
+author: Myshko E.V.
+"""
+
+
+class Voice:
+    """
+    This object represents a voice message.
+    """
+
+    file_id = None
+    duration = None
+    mime_type = None
+    file_size = None
+
+
+    def __init__(self, voice_data: dict):
+        """
+        Object initialization
+        :param voice_data: The voice data that was received from the update object api telegram
+        """
+        pass
